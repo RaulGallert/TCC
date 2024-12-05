2e3d59fe3635c54e652592db7a2b294c9bc5133a
RaulGallert <raulgallertseverino@gmail.com>
2024-12-05 08:54:34 -0300

    oi

--- js/vr.js
+++ js/vr.js
@@ -3,7 +3,15 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
     // Selector '#vrview' finds element with id 'vrview'.
     var vrView = new VRView.Player('#teste', {
-        image: 'img/gallery/panorama.jpg',
+        image: 'img/gallery/machu-picchu-preview.jpg',
+        is_stereo: true
+      });    
+  }  
+
+function onVrViewLoad() {
+    // Selector '#vrview' finds element with id 'vrview'.
+    var vrView = new VRView.Player('#sala', {
+        image: 'img/gallery/sala.jpg',
         is_stereo: true
       });    
   }  
